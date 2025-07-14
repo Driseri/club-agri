25d595abbadd1595464ddee45aa961c469f125a9
Yaschenko Daniil <52747544+Driseri@users.noreply.github.com>
2025-07-14 21:42:48 +0300

    Add start screen with intro

---
+++ client/src/presentation/screens/Start.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import { onMounted, onBeforeUnmount } from 'vue'
+import { useTelegram } from '@/application/services'
+import { useRouter } from 'vue-router'
+import { Placeholder } from '@/presentation/components'
+
+const { showMainButton, hideMainButton, expand } = useTelegram()
+const router = useRouter()
+
+onMounted(() => {
+  expand()
+  showMainButton('Начать', () => {
+    void router.push('/home')
+  })
+})
+
+onBeforeUnmount(() => {
+  hideMainButton()
+})
+</script>
+
+<template>
+  <div class="start-page">
+    <Placeholder
+      title="Club Agri"
+      caption="Этот сервис позволяет подобрать удобные клубы поблизости"
+    >
+      <template #picture>
+        <img src="/telebook.svg" aria-hidden="true" width="80" />
+      </template>
+    </Placeholder>
+  </div>
+</template>
+
+<style scoped>
+.start-page {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100%;
+}
+</style>
